381a30607953973c4a32b283141c294ba5dec1fd
John James <jjames@phantomhost.net>
2026-09-14 14:45:27 -0500

    Use Set-DnsClientServerAddress instead of netsh for DNS

--- Network/set-wired-static.ps1
+++ Network/set-wired-static.ps1
@@ -41,10 +41,9 @@ Get-NetRoute -InterfaceIndex $wired.InterfaceIndex -ErrorAction SilentlyContinue
 Write-Host "Setting static IP $ip via netsh..."
 netsh interface ip set address name="$($wired.Name)" source=static addr=$ip mask=255.255.255.0
 
-# Set DNS servers
+# Set DNS servers using PowerShell cmdlet (more reliable than netsh for names with spaces)
 Write-Host "Setting DNS servers..."
-netsh interface ip set dns name="$($wired.Name)" source=static addr=4.2.2.2
-netsh interface ip add dns name="$($wired.Name)" addr=8.8.8.8 index=2
+Set-DnsClientServerAddress -InterfaceIndex $wired.InterfaceIndex -ServerAddresses ("4.2.2.2", "8.8.8.8")
 
 Start-Sleep -Seconds 2
 
